91afd5042097a25dbdc95eac511611eddb126e05
Zhong Zhenyu <nczzy1997@gmail.com>
2019-05-04 12:13:52 +0800

    更改关卡选择逻辑

--- Klotski/Assets/Scripts/Scene 1/StageScrollView.cs
+++ Klotski/Assets/Scripts/Scene 1/StageScrollView.cs
@@ -1,4 +1,5 @@
 ﻿using Common;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,8 +10,15 @@ namespace Scene_1
         [SerializeField] private StageConfig[] stageConfigs;
         [SerializeField] private GameObject scrollContent;
         [SerializeField] private GameObject stageItem;
+        [SerializeField] private GameObject selectIndicator;
         [SerializeField] private float startingPosition;
         [SerializeField] private StageSelectorAnimator animator;
+        [SerializeField] private GameObject playButton;
+        [SerializeField] private GameObject steps;
+        [SerializeField] private GameObject time;
+        [SerializeField] private GameObject unfinished;
+
+        private GameObject mCurrentSelectIndicator;
 
         private void Start()
         {
@@ -22,9 +30,12 @@ namespace Scene_1
                 stageItemObject.GetComponentInChildren<Text>().text = stageConfigs[i].GetStageName();
                 stageItemObject.GetComponent<RectTransform>().anchoredPosition =
                     // ReSharper disable once PossibleLossOfFraction
-                    new Vector3((float) (i % 2 == 0 ? -232.52 : 232.52), startingPosition - i / 2 * itemHeight, 0);
+                    new Vector3((float) (i % 2 == 0 ? -232.52 : 235), startingPosition - i / 2 * itemHeight, 0);
                 var stageConfig = stageConfigs[i];
-                stageItemObject.GetComponent<Button>().onClick.AddListener(() => { animator.LoadStage(stageConfig); });
+                stageItemObject.GetComponent<Button>().onClick.AddListener(() =>
+                {
+                    StageItemSelected(stageItemObject, stageConfig);
+                });
             }
 
             scrollContent.GetComponent<RectTransform>().sizeDelta =
@@ -42,6 +53,44 @@ namespace Scene_1
         private void OnDestroy()
         {
             Store.LastScrollPosition = scrollContent.GetComponent<RectTransform>().localPosition.y;
+        }
+
+        public void RandomGame()
+        {
+            animator.LoadStage(stageConfigs[new System.Random().Next(stageConfigs.Length)]);
+        }
+
+        private void StageItemSelected(GameObject stageItemObject, StageConfig stageConfig)
+        {
+            if (mCurrentSelectIndicator != null)
+            {
+                Destroy(mCurrentSelectIndicator);
+            }
+
+            mCurrentSelectIndicator = Instantiate(selectIndicator, stageItemObject.transform);
+            mCurrentSelectIndicator.GetComponent<RectTransform>().localPosition = new Vector3(0, -56, 0);
+
+            playButton.GetComponentInChildren<TextMeshProUGUI>().text = "\uf04b";
+            playButton.GetComponent<Button>().onClick.RemoveAllListeners();
+            playButton.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                animator.LoadStage(stageConfig);
+            });
+
+            if (true)
+            {
+                unfinished.SetActive(false);
+                steps.SetActive(true);
+                time.SetActive(true);
+                steps.transform.GetChild(0).GetComponent<Text>().text = "68";
+                time.transform.GetChild(0).GetComponent<Text>().text = "00:35:00";
+            }
+            else
+            {
+                unfinished.SetActive(true);
+                steps.SetActive(false);
+                time.SetActive(false);
+            }
         }
     }
 }
