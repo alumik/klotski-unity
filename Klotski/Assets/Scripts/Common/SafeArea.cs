a5f7e5bb85b90fbbbc5b55bba848581c8bd4dd83
Zhong Zhenyu <nczzy1997@gmail.com>
2019-05-04 23:19:49 +0800

    处理屏幕安全区问题

---
+++ Klotski/Assets/Scripts/Common/SafeArea.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public class SafeArea : MonoBehaviour
+    {
+        private RectTransform mPanel;
+        private Rect mLastSafeArea = new Rect(0, 0, 0, 0);
+
+        private void Awake()
+        {
+            mPanel = GetComponent<RectTransform>();
+            Refresh();
+        }
+
+        private void Update()
+        {
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            var safeArea = GetSafeArea();
+            if (safeArea != mLastSafeArea)
+            {
+                ApplySafeArea(safeArea);
+            }
+        }
+
+        private static Rect GetSafeArea()
+        {
+            return Screen.safeArea;
+        }
+
+        private void ApplySafeArea(Rect r)
+        {
+            mLastSafeArea = r;
+            var anchorMin = r.position;
+            var anchorMax = r.position + r.size;
+            anchorMin.x /= Screen.width;
+            anchorMin.y /= Screen.height;
+            anchorMax.x /= Screen.width;
+            anchorMax.y /= Screen.height;
+            mPanel.anchorMin = anchorMin;
+            mPanel.anchorMax = anchorMax;
+        }
+    }
+}
