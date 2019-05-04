2889506330971a2c6b438eab9e2572ce339b57f9
Zhong Zhenyu <nczzy1997@gmail.com>
2019-05-05 00:29:47 +0800

    适配窄屏

---
+++ Klotski/Assets/Scripts/Common/ScaleRelativeToCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public class ScaleRelativeToCamera : MonoBehaviour 
+    {
+        public Camera cam; 
+        public float objectScale = 1.0f; 
+        private Vector3 initialScale; 
+ 
+        // set the initial scale, and setup reference camera
+        void Start ()
+        {
+            // record initial scale, use this as a basis
+            initialScale = transform.localScale; 
+ 
+            // if no specific camera, grab the default camera
+            if (cam == null)
+                cam = Camera.main; 
+            Plane plane = new Plane(cam.transform.forward, cam.transform.position); 
+            float dist = plane.GetDistanceToPoint(transform.position); 
+            transform.localScale = initialScale * dist * objectScale; 
+        }
+    }
+}
