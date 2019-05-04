c8c5da1e8583e09d297686e46df72815802e7517
Zhong Zhenyu <nczzy1997@gmail.com>
2019-05-04 14:24:05 +0800

    微调 UI

--- Klotski/Assets/Scripts/Scene 3/Win.cs
+++ Klotski/Assets/Scripts/Scene 3/Win.cs
@@ -9,6 +9,8 @@ namespace Scene_3
     {
         [SerializeField] private Text stepCounter;
         [SerializeField] private Text time;
+        [SerializeField] private Text bestStepCounter;
+        [SerializeField] private Text bestTime;
         [SerializeField] private Text stageName;
 
         private void Start()
@@ -18,12 +20,20 @@ namespace Scene_3
                 Camera.main.backgroundColor = Store.CurrentColor;
             }
 
-            Store.Db.SaveResult(Store.NextStageConfig.GetStageId(), Store.Steps, Store.Time);
+            var stageConfig = Store.NextStageConfig;
+
+            Store.Db.SaveResult(stageConfig.GetStageId(), Store.Steps, Store.Time);
             var timeSpan = TimeSpan.FromSeconds(Store.Time);
             var timeText = $"{timeSpan.Hours:D2}:{timeSpan.Minutes:D2}:{timeSpan.Seconds:D2}";
             time.text = "用时 " + timeText;
             stepCounter.text = "总步数 " + Store.Steps;
-            stageName.text = Store.NextStageConfig.GetStageName();
+            stageName.text = stageConfig.GetStageName();
+            
+            var result = Store.Db.GetResult(stageConfig.GetStageId());
+            bestStepCounter.text = result.BestSteps.ToString();
+            timeSpan = TimeSpan.FromSeconds(result.BestTime);
+            timeText = $"{timeSpan.Hours:D2}:{timeSpan.Minutes:D2}:{timeSpan.Seconds:D2}";
+            bestTime.text = timeText;
         }
 
         private void Update()
