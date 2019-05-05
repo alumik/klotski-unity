3abffaef9854addf46c4008d1933df867c41b2fc
Zhong Zhenyu <nczzy1997@gmail.com>
2019-05-05 21:05:29 +0800

    大幅缩小 APK 大小

--- Klotski/Assets/Scripts/Scene 1/StageSelector.cs
+++ Klotski/Assets/Scripts/Scene 1/StageSelector.cs
@@ -1,21 +1,16 @@
 ﻿using Common;
-using Scene_0;
-using TMPro;
 using UnityEngine;
 
 namespace Scene_1
 {
     public class StageSelector : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI bgmButton;
-
         private void Start()
         {
             if (Camera.main != null)
             {
                 Camera.main.backgroundColor = Store.CurrentColor;
             }
-            ChangeBgmButton();
         }
 
         private void Update()
@@ -23,14 +18,6 @@ namespace Scene_1
             if (Input.GetKeyDown("escape"))
             {
                 GetComponent<StageSelectorAnimator>().Back();
-            }
-        }
-
-        private void ChangeBgmButton()
-        {
-            if (BackgroundMusic.Instance != null)
-            {
-                bgmButton.text = BackgroundMusic.Instance.GetComponent<AudioSource>().isPlaying ? "\uf026" : "\uf6a9";
             }
         }
     }
